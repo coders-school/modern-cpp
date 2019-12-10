b568ccd594e42a48879b04e717df2b6fea800c3e
Michal Piotrowski <michal.piotrowski@redembedded.com>
2019-12-10 15:47:56 +0100

    Post_work

--- Circle.cpp
+++ Circle.cpp
@@ -2,8 +2,8 @@
 #include <math.h>
 #include <iostream>
 
-Circle::Circle(double r)
-    : r_(r)
+Circle::Circle(double r, Color col)
+    : r_(r), color(col)
 {}
 
 Circle::Circle(const Circle & other)
@@ -11,12 +11,12 @@ Circle::Circle(const Circle & other)
     r_ = other.getRadius();
 }
 
-double Circle::getArea() const
+double Circle::getArea() const noexcept
 {
     return M_PI * r_ * r_;
 }
 
-double Circle::getPerimeter() const
+double Circle::getPerimeter() const noexcept
 {
     return 2 * M_PI * r_;
 }
@@ -26,9 +26,26 @@ double Circle::getRadius() const
     return r_;
 }
 
+
+double Circle::getPi() const
+{
+    return M_PI;
+}
+
+std::string Circle::getColorName() const
+{
+    switch (color) {
+    case Color::Red: return "Red";
+    case Color::Green: return "Green";
+    case Color::Blue: return "Blue";
+    }
+    return "Undefined color";
+}
+
 void Circle::print() const
 {
     std::cout << "Circle: radius: " << getRadius() << std::endl
               << "          area: " << getArea() << std::endl
-              << "     perimeter: " << getPerimeter() << std::endl;
+              << "     perimeter: " << getPerimeter() << std::endl
+              << "         color: " << getColorName() << std::endl;
 }
