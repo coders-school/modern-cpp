e08e260d678ea2b96ab3ef5a33400fd063de5828
Łukasz Ziobroń <lukasz@coders.school>
2020-10-08 21:20:00 +0200

    Exercises done during classes

--- shapes/Square.cpp
+++ shapes/Square.cpp
@@ -1,25 +1,23 @@
 #include "Square.hpp"
 #include <iostream>
 
+Square::Square(Color c) : Rectangle(c) {}
+
 Square::Square(double x)
     : Rectangle(x, x)
 {}
 
-Square::Square(const Square &other)
-    : Rectangle(other.getX(), other.getX())
-{}
-
-double Square::getArea()
+double Square::getArea() const noexcept
 {
     return getX() * getX();
 }
 
-double Square::getPerimeter()
+double Square::getPerimeter() const noexcept
 {
     return 4 * getX();
 }
 
-void Square::print()
+void Square::print() const
 {
     std::cout << "Square:      x: " << getX() << std::endl
               << "          area: " << getArea() << std::endl
