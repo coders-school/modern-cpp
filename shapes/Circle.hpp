dde22d9b7a99ad5da88a2d074591c49b84c47d4c
Kasper Biegun <kasper.biegun95@gmail.com>
2020-10-13 14:27:38 +0200

    Alignas(128) used in Circle class

--- shapes/Circle.hpp
+++ shapes/Circle.hpp
@@ -2,22 +2,20 @@
 
 #include "Shape.hpp"
 
-class Circle final : public Shape
-{
+class alignas(128) Circle final : public Shape {
 public:
     using Shape::Shape;
     Circle(double r);
-    Circle(const Circle & other) = default;
+    Circle(const Circle& other) = default;
 
-    [[deprecated("Please use M_PI instead.")]]
-    double getPi() const;
+    [[deprecated("Please use M_PI instead.")]] double getPi() const;
     double getArea() const override;
     double getPerimeter() const override;
     auto getRadius() const;
     void print() const override;
 
 private:
-    Circle() = delete; // doesn't allow to call default constructor
+    Circle() = delete;  // doesn't allow to call default constructor
 
     double r_{};
 };
--- shapes/main.cpp
+++ shapes/main.cpp
@@ -1,86 +1,72 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <string>
+#include <iostream>
 #include <memory>
-#include "Shape.hpp"
-#include "Rectangle.hpp"
-#include "Square.hpp"
+#include <string>
+#include <vector>
 #include "Circle.hpp"
+#include "Rectangle.hpp"
+#include "Shape.hpp"
+#include "Square.hpp"
 
 using namespace std;
 
 using Collection = vector<shared_ptr<Shape>>;
 
-bool sortByArea(shared_ptr<Shape> first, shared_ptr<Shape> second)
-{
-    if(first == nullptr || second == nullptr)
+bool sortByArea(shared_ptr<Shape> first, shared_ptr<Shape> second) {
+    if (first == nullptr || second == nullptr)
         return false;
     return (first->getArea() < second->getArea());
 }
 
-bool perimeterBiggerThan20(shared_ptr<Shape> s)
-{
-    if(s)
+bool perimeterBiggerThan20(shared_ptr<Shape> s) {
+    if (s)
         return (s->getPerimeter() > 20);
     return false;
 }
 
-bool areaLessThan10(shared_ptr<Shape> s)
-{
-    if(s)
+bool areaLessThan10(shared_ptr<Shape> s) {
+    if (s)
         return (s->getArea() < 10);
     return false;
 }
 
-void printCollection(const Collection& collection)
-{
-    for (const auto & it : collection)
+void printCollection(const Collection& collection) {
+    for (const auto& it : collection)
         if (it)
             it->print();
 }
 
-void printAreas(const Collection& collection)
-{
-    for (const auto & it : collection)
+void printAreas(const Collection& collection) {
+    for (const auto& it : collection)
         if (it)
             cout << it->getArea() << std::endl;
 }
 
 void findFirstShapeMatchingPredicate(const Collection& collection,
                                      bool (*predicate)(shared_ptr<Shape> s),
-                                     std::string info)
-{
+                                     std::string info) {
     auto iter = std::find_if(collection.begin(), collection.end(), predicate);
-    if(*iter != nullptr)
-    {
+    if (*iter != nullptr) {
         cout << "First shape matching predicate: " << info << endl;
         (*iter)->print();
-    }
-    else
-    {
+    } else {
         cout << "There is no shape matching predicate " << info << endl;
     }
 }
 
 constexpr int fibo(int n) {
-    if (n<=2) {
+    if (n <= 2) {
         return 1;
     } else {
         return fibo(n - 1) + fibo(n - 2);
     }
 }
 
-int main()
-{
+int main() {
     constexpr int n = fibo(45);
-    Collection shapes {
-        make_shared<Circle>(2.0),
-        make_shared<Circle>(3.0),
-        nullptr,
-        make_shared<Circle>(4.0),
-        make_shared<Rectangle>(10.0, 5.0),
-        make_shared<Square>(3.0),
+    Collection shapes{
+        make_shared<Circle>(2.0), make_shared<Circle>(3.0),          nullptr,
+        make_shared<Circle>(4.0), make_shared<Rectangle>(10.0, 5.0), make_shared<Square>(3.0),
         make_shared<Circle>(4.0),
     };
     printCollection(shapes);
@@ -105,6 +91,12 @@ int main()
 
     findFirstShapeMatchingPredicate(shapes, perimeterBiggerThan20, "perimeter bigger than 20");
     findFirstShapeMatchingPredicate(shapes, areaLessThan10, "area less than 10");
+
+    std::cout << "\n\n***** HOMEWORK PART *****\n";
+
+    std::cout << "Circle alignment is equal to: " << alignof(Circle) << '\n';
+    std::cout << "When alignment was set to alignas(2), Circle object was aligned by 8. VSC showed a warning that "
+                 "\"alignment cannot be set to less than the default alignment\"\n";
 
     return 0;
 }
