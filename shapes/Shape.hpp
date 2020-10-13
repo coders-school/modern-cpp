9e3ff6174e2a49eed15837d8f5ddb0ee767820e8
Vicx95 <wiktor.zyla31@gmail.com>
2020-10-13 20:54:18 +0200

    add enum with colors

--- shapes/Shape.hpp
+++ shapes/Shape.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+enum class Color : unsigned char{
+    Red,
+    Green,
+    Black
+};
+
 class Shape
 {
 public:
@@ -8,4 +14,6 @@ public:
     virtual double getArea() const = 0;
     virtual double getPerimeter() const = 0;
     virtual void print() const;
+private:
+    Color color_ = Color::Green;
 };
