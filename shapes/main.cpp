57a8db5e839310eff33e6474d78bc42e9b632ff6
Łukasz Ziobroń <lukasz@coders.school>
2022-01-31 10:13:38 +0100

    Update main.cpp

--- shapes/main.cpp
+++ shapes/main.cpp
@@ -14,7 +14,7 @@ typedef vector<shared_ptr<Shape>> Collection;
 
 bool sortByArea(shared_ptr<Shape> first, shared_ptr<Shape> second)
 {
-    if(first == nullptr || second == nullptr)
+    if(first == NULL || second == NULL)
         return false;
     return (first->getArea() < second->getArea());
 }
@@ -52,7 +52,7 @@ void findFirstShapeMatchingPredicate(const Collection& collection,
                                      std::string info)
 {
     Collection::const_iterator iter = std::find_if(collection.begin(), collection.end(), predicate);
-    if(*iter != nullptr)
+    if(*iter != 0)
     {
         cout << "First shape matching predicate: " << info << endl;
         (*iter)->print();
